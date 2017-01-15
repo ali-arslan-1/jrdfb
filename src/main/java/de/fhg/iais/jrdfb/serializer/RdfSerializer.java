42f1c32776f006f2876f704b5a8da78c5569054d
Ali Arslan <ali.arslan@rwth-aachen.de>
2017-01-15 14:33:53 +0100

    Deserializer now can call private constructors.

--- src/main/java/de/fhg/iais/jrdfb/serializer/RdfSerializer.java
+++ src/main/java/de/fhg/iais/jrdfb/serializer/RdfSerializer.java
@@ -14,6 +14,7 @@ import org.apache.jena.vocabulary.VOID;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.UnsupportedEncodingException;
+import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
 import java.util.UUID;
 
@@ -102,7 +103,9 @@ public class RdfSerializer {
 
         Class rootClass = tClasses[0]; //todo
 
-        Object obj = rootClass.newInstance();
+        Constructor cons = rootClass.getDeclaredConstructor();
+        cons.setAccessible(true);
+        Object obj = cons.newInstance();
 
         String RDFType;
         if(rootClass.isAnnotationPresent(RdfType.class))
